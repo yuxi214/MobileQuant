95968b2b298fc8b278feabbd32d6d644b4c07523
liulin <459057268@qq.com>
2018-06-28 10:50:23 +0800

    初始版本

--- MobileQuant_App/app/src/main/java/com/mquant/mobilequant/MainActivity.java
+++ MobileQuant_App/app/src/main/java/com/mquant/mobilequant/MainActivity.java
@@ -17,14 +17,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_strategy:
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_account:
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_setting:
                     return true;
             }
             return false;
